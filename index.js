cbb005d03a48a7cb8a621b864dc6fa4bb92222ba
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-11 01:05:13 +0530

    six comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 app.get("/testing",(req,res)=>{
     return res.status(200).json({
-        message:"jenkins125"
+        message:"wap"
     })
 })
 app.get((req,res,next)=>{
