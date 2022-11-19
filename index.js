6833349ea7cc4b993fb750b611e74cfa04df95f4
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-19 16:10:38 +0530

    m

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 app.get("/testing",(req,res)=>{
     return res.status(200).json({
-        message:"wap institute master"
+        message:"wap institute master data"
     })
 })
 app.get((req,res,next)=>{
