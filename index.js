368bfb52045fc163ec92a27a94c45669afceb8c8
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-19 15:10:00 +0530

    16 commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 app.get("/testing",(req,res)=>{
     return res.status(200).json({
-        message:"wap1522"
+        message:"wap institue"
     })
 })
 app.get((req,res,next)=>{
