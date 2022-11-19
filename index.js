787435c12f9a3282296ff17b50622b0de66a8ef1
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-19 16:16:51 +0530

    set

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 app.get("/testing",(req,res)=>{
     return res.status(200).json({
-        message:"wap institute 125"
+        message:"wap institute 1256"
     })
 })
 app.get((req,res,next)=>{
