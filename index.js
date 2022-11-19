85795dfa792b2a37a31846bb326bb1e50dda2f05
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-19 17:15:22 +0530

    add testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 app.get("/testing",(req,res)=>{
     return res.status(200).json({
-        message:"wap institute"
+        message:"wap institute testing"
     })
 })
 app.get((req,res,next)=>{
