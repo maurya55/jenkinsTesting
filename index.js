db651c7abe5e2955ba720fdbb0dfb2fe550d7237
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-19 16:40:18 +0530

    add file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+app.get("/testing",(req,res)=>{
+    return res.status(200).json({
+        message:"wap institute master"
+    })
+})
+app.get((req,res,next)=>{
+    console.log("not found")
+})
+app.get((err,req,res,next)=>{
+    console.log(err); 
+    // console.log("not found")
+})
+
+app.listen(8000,()=>{
+    console.log("connected")
+})
